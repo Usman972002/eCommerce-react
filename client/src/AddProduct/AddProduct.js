a7af36092d6f4d52a6849911ec81131383e5e061
Usman972002 <101034083+Usman972002@users.noreply.github.com>
2023-08-05 21:06:58 +0530

    tried to add the image for uploading while adding a product

--- client/src/AddProduct/AddProduct.js
+++ client/src/AddProduct/AddProduct.js
@@ -1,40 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddProduct.css'
+import axios from 'axios'
 
 const AddProduct = () => {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [subcategory, SetSubcategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(null);
+  const [brand,setBrand] = useState("");
+  const [size,setSize] = useState("");
+  const [image,setImage] = useState(null);
+
+  const handleSave = async () =>{
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('category', category);
+    formData.append('subcategory', subcategory);
+    formData.append('description', description);
+    formData.append('price', price);
+    formData.append('brand', brand);
+    formData.append('size', size);
+    formData.append('image', image);
+
+    try {
+      await axios.post('/addProduct', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      // Product added successfully, you can perform any necessary actions here
+    } catch (error) {
+      // Handle error
+      console.error('Error adding product:', error);
+    }
+  };
+
   return (
     <div className='product__page'>
-        <div className='add__product__form'>
-            <h1>
-                Add Product
-            </h1>
-            <div className='param__1'>
-                <label>Title</label>
-                <input type='text' className='product__title'></input>
-                </div>
-                <div className='param__2'>
-                <label>Category</label>
-                <input type='text' className='product__category'></input>
-                </div>
-                <div className='param__3'>
-                <label>Sub category</label>
-                <input type='text' className='product__subcategory'></input>
-                </div>
-                <div className='param__4'>
-                <label>Description</label>
-                {/* <input type='text' className='product__Description'></input> */}
-                <textarea className='product__description'></textarea>
-                </div>
-                <div className='param__5'>
-                <label>Price</label>
-                <input type='text' className='product__price'></input>
-                </div>
-                <div className='param__6'>
-                <label>Image</label>
-                <input type="file" accept="image/*" className='product__image'></input>
-                </div>
-                <button className='save__button'>Save Product</button>                
+      <div className='add__product__form'>
+        <h1>
+          Add Product
+        </h1>
+        <div className='param__1'>
+          <label>Title</label>
+          <input type='text' className='product__title' onChange={(e)=>{setTitle(e.target.value)}}></input>
         </div>
+        <div className='param__7'>
+          <label>Brand</label>
+          <input type='text' className='product__brand' onChange={(e)=>{setBrand(e.target.value)}}></input>
+        </div>
+        <div className='param__2'>
+          <label>Category</label>
+          <input type='text' className='product__category' onChange={(e)=>{setCategory(e.target.value)}}></input>
+        </div>
+        <div className='param__3'>
+          <label>Sub category</label>
+          <input type='text' className='product__subcategory' onChange={(e)=>{SetSubcategory(e.target.value)}}></input>
+        </div>
+        <div className='param__8'>
+          <label>Size</label>
+          <input type='text' className='product__size' onChange={(e)=>{setSize(e.target.value)}}></input>
+        </div>
+        <div className='param__4'>
+          <label>Description</label>
+          <textarea className='product__description' onChange={(e)=>{setDescription(e.target.value)}}></textarea>
+        </div>
+        <div className='param__5'>
+          <label>Price</label>
+          <input type='text' className='product__price' onChange={(e)=>{setPrice(e.target.value)}}></input>
+        </div>
+        <div className='param__6'>
+          <label>Image</label>
+          <input type="file" accept="image/*" className='product__image' onChange={(e)=>{setImage(e.target.files[0])}}></input>
+        </div>
+        <button className='save__button' onClick={handleSave}>Save Product</button>
+      </div>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const app = express();
 const port = 4000;
 app.use(express.json());
 require('dotenv').config();
-
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+const multer = require('multer'); // Import Multer for handling file uploads
+const storage = multer.memoryStorage(); // Store files in memory
+const upload = multer({ storage });
 
 async function connectToDatabase() {
   try {
@@ -38,6 +40,41 @@ app.post('/addPost',async(req,res)=>{
     res.status(500).json({error:"The insrtion has been failed"});
   }
 })
+
+// app.post('/addProduct',async(req,res)=>{
+//   const productDetails = req.body;
+//   try{
+//     const addedProduct = await client.db('newDb').collection('posts').insertOne(productDetails);
+//     res.status(200).json(addedProduct);
+//   }catch(error){
+//     res.status(500).json({error:"There was an error while adding a product"})
+//   }
+// })
+
+app.post('/addProduct', upload.single('image'), async (req, res) => {
+  try {
+    const { title, category, subcategory, description, price, brand, size } = req.body;
+    const imageFile = req.file; // Access the uploaded image file
+
+    // Process the image file here, you can save it to a server directory or a cloud storage service
+
+    const store = {
+      title,
+      category,
+      subcategory,
+      description,
+      price,
+      brand,
+      size,
+      imageUrl: 'path/to/your/image' // Update this with the actual URL of the saved image
+    };
+
+    const addedProduct = await client.db('newDb').collection('products').insertOne(store);
+    res.status(200).json(addedProduct);
+  } catch (error) {
+    res.status(500).json({ error: "The insertion has failed" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
